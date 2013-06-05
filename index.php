fd55ab548cec5cd267a834591bdbfc7108ad01d8
Nicolas Baudet <nicolas.baudet@heig-vd.ch>
2013-06-05 09:46:22 +0200

    Commit initial
    Installation de la structure des fichiers, création des fichiers de base
    dans chaque dossier.

---
+++ index.php
@@ -0,0 +1,67 @@
+﻿<html>
+	<head>
+		<style>
+			body {
+				font-family: Helvetica, Verdana;
+			}
+			#wrapper {
+				width:1290px;
+				margin:0px auto;
+			}
+			header {
+				border-bottom: 2px solid #ff3300;
+				text-align:center;
+			}
+			footer {
+				border-top : 2px solid #ff3300;
+				font-style:italic;
+			}
+			
+			#content {
+				margin: 20px 0px;
+			}
+			#content .groupe {
+				width:400px;
+				float:left;
+				border:1px solid lightgrey;
+				margin-right:27px;
+				min-height:300px;
+				display:block;
+				
+				padding:5px;
+			}
+			a {
+				color:#ff3300;
+			}
+		</style>
+	</head>
+	<body>
+		<section id="wrapper">
+			<header>
+				<img src="./Common/logo_TEDx_Events_Manager.png" alt="TEDx Events Manager" />
+				<p>Sélectionnez un template réalisé par les classes médias</p>
+			</header>
+			<section id="content">
+				<section class="groupe">
+					<a href="index_group1.php">
+						Accéder au template du groupe 1
+					</a>
+				</section>
+				<section class="groupe">
+					<a href="index_group2.php">
+						Accéder au template du groupe 2
+					</a>
+				</section>
+				<section class="groupe" style="margin-right:0px">
+					<a href="index_group3.php">
+						Accéder au template du groupe 3
+					</a>
+				</section>
+				<div style="clear:both"></div>
+			</section>
+			<footer>
+				<p>Une application Web réalisé par étudiants COMEM+ en 2ème année.</p>
+			</footer>
+		</section>
+	</body>
+</html>
---
+++ index_group2.php
@@ -0,0 +1,1 @@
+template groupe 2
