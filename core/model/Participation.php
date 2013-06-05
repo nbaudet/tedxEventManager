dcb04163326fd5cdfa29a06752777eee7f3552b2
spacefiller <guillaume.lehmann@heig-vd.ch>
2013-06-05 16:43:56 +0200

    new class Participation.php

---
+++ core/model/Participation.php
@@ -0,0 +1,117 @@
+<?php
+
+
+/**
+ * Participation.class.php
+ * 
+ * Author : Guillaume Lehmann
+ * Date : 05.06.2013
+ * 
+ * Description : define the class Participation as definited in the model
+ * 
+ */
+class Participation {
+    
+    
+    /**
+     * Participation's slotNo
+     * @var type int 
+     */
+    private $slotNo; 
+    
+    
+    /**
+     * Participation's slotEventNo
+     * @var type int 
+     */
+    private $slotEventNo; 
+    
+    
+    /**
+     * Participation's participantPersonNo
+     * @var type int 
+     */
+    private $participantPersonNo; 
+    
+    
+    /**
+     * Participation's isArchived
+     * @var type boolean
+     */
+    private $isArchived; 
+    
+    
+    
+    /**
+     * Constructs object Participation
+     * 
+     * @param type $array of parameters that correspond to the classes properties
+     */
+    public function __construct($array = null){
+        
+        if(!is_array($array)) {
+            throw new Exception('No parameters');
+            
+        }//if
+        $this->slotNo = $array['slotNo']; 
+        $this->slotEventNo = $array['slotEventNo']; 
+        $this->participantPersonNo = $array['participantPersonNo']; 
+        $this->isArchived = $array['isArchived']; 
+        
+
+    
+
+        
+    }//construct
+    
+    
+    
+    
+    
+    /**
+     * get slotNo
+     * @return type int slotNo
+     */
+    public function getSlotNo() {
+        return $this->slotNo; 
+    }
+    
+    
+    /**
+     * get slotEventNo
+     * @return type int slotEventNo
+     */
+    public function getSlotEventNo() {
+        return $this->slotEventNo; 
+    }
+    
+    
+    /**
+     * get participantPersonNo
+     * @return type int participantPersonNo
+     */
+    public function getParticipantPersonNo() {
+        return $this->participantPersonNo; 
+    }
+    
+    
+    /**
+     * get isArchived
+     * @return type boolean isArchived
+     */
+    public function getIsArchived() {
+        return $this->isArchived; 
+    }
+    
+    
+    /**
+     * set isArchived
+     * @param type $isArchived 
+     */
+    public function setIsArchived($isArchived) {
+        $this->isArchived = $isArchived; 
+    }
+    
+    
+}//class
+?>
