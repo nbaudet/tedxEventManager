f90a5f997fe85d123604435a251648fa498c5a08
Francelet L <lauric.francelet@heig-vd.ch>
2013-06-12 16:33:56 +0200

    Change FSPerson.php

--- core/services/functionnals/fsPerson.php
+++ core/services/functionnals/fsPerson.php
@@ -9,7 +9,7 @@ class FSPerson {
     /**
      * Returns a Person with the given No as Id.
      * @param int $no The Id of the Person
-     * @return a Message with an existant Person
+     * @return a Message containing an existant Person
      */
     protected function getPerson($no) {
         $person = NULL;
@@ -17,7 +17,7 @@ class FSPerson {
         global $crud;
         
         $sql = "SELECT * FROM person WHERE no = $no";
-        $data = $crud->query($sql);
+        $data = $crud->getRow($sql);
         
         if($data){
             $argsPerson = array(
@@ -48,7 +48,7 @@ class FSPerson {
             $argsMessage = array(
                 'messageNumber' => 102,
                 'message'       => 'Inexistant Person',
-                'status'        => true,
+                'status'        => false,
                 'content'       => NULL
             );
             $message = new Message($argsMessage);
@@ -56,6 +56,108 @@ class FSPerson {
         }
     }
     
+    /**
+     * Returns all the Persons of the database
+     * @return A Message containing an array of Persons
+     */
+    public function getPersons(){
+        global $crud;
+        
+        $sql = "SELECT * FROM person";
+        $data = $crud->getRows($sql);
+        
+        if ($data){
+            $persons = array();
+
+            
+            foreach($data as $row){
+                $argsPerson = array(
+                    'no'            => $row['no'],
+                    'name'          => $row['name'],
+                    'firstname'     => $row['firstname'],
+                    'dateOfBirth'   => $row['dateOfBirth'],
+                    'address'       => $row['address'],
+                    'city'          => $row['city'],
+                    'country'       => $row['country'],
+                    'phoneNumber'   => $row['phoneNumber'],
+                    'email'         => $row['email'],
+                    'description'   => $row['description'],
+                    'isArchived'    => $row['isArchived']
+                );
+            
+                $persons[] = new Person($argsPerson);
+            } //foreach
+
+            $argsMessage = array(
+                'messageNumber' => 103,
+                'message'       => 'All Persons selected',
+                'status'        => true,
+                'content'       => $persons
+            );
+            $message = new Message($argsMessage);
+
+            return $message;
+        } else {
+            $argsMessage = array(
+                'messageNumber' => 104,
+                'message'       => 'Error while SELECT * FROM person',
+                'status'        => false,
+                'content'       => NULL
+            );
+            $message = new Message($argsMessage);
+
+            return $message;
+        }
+    }
+    
+    /**
+     * Add a new Person in Database
+     * @param $args Parameters of a Person
+     * @return a Message containing the new Person
+     */
+    public function addPerson($args){
+        global $crud;
+        
+        $sql = "INSERT INTO `tedx`.`person` (
+            `No`, `Name`, `Firstname`, `DateOfBirth`, `Address`, `City`, 
+            `Country`, `PhoneNumber`, `Email`, `Description`, `IsArchived`) VALUES (
+                NULL, 
+                '".$args['name']."', 
+                '".$args['firstname']."', 
+                '".$args['dateOfBirth']."', 
+                '".$args['address']."', 
+                '".$args['city']."',
+                '".$args['country']."',
+                '".$args['phoneNumber']."', 
+                '".$args['email']."',
+                '".$args['description']."',
+                '".$args['isArchived']."'
+        );";
+        
+        if($crud->exec($sql)){
+            
+            
+            $argsMessage = array(
+                'messageNumber' => 105,
+                'message'       => 'New Person added !',
+                'status'        => true,
+                'content'       => 1
+            );
+            $message = new Message($argsMessage);
+            return $message;
+        } else {
+            $argsMessage = array(
+                'messageNumber' => 106,
+                'message'       => 'Error while inserting new Person',
+                'status'        => false,
+                'content'       => NULL
+            );
+            $message = new Message($argsMessage);
+
+            return $message;
+        }
+        
+    }
     
 }
 
