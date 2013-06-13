d4433c3811c19aea46d3b7b2405cce3798c2c715
Raphaël Schmutz <raphael.schmutz@heig-vd.ch>
2013-06-13 11:36:33 +0200

    FS Unit avec 2 get
    getById et getByName

--- dev/RS_STUB.php
+++ dev/RS_STUB.php
@@ -4,11 +4,14 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+require_once('../core/model/Unit.class.php');
 require_once('../core/model/message.class.php');
+require_once('../core/services/functionnals/FSUnit.class.php');
 require_once('../tedx-config.php');
 
+$message = FSUnit::getUnitByName('Visitors');
+//$args = array();
 
-$args = array();
-$message = $tedx_manager->registerVisitor($args);
+// $message = $tedx_manager->registerVisitor($args);
 print_r($message);
 ?>
