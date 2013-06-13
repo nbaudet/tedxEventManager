6b2663a55011cf98ae1cec70bce9e14e3e2e086d
Francelet L <lauric.francelet@heig-vd.ch>
2013-06-13 20:25:20 +0200

    Changes to FSSlot

---
+++ core/services/functionnals/FSSlot.class.php
@@ -0,0 +1,244 @@
+<?php
+require_once(APP_DIR . '/core/model/Slot.class.php');
+require_once(APP_DIR . '/core/model/Message.class.php');
+
+/**
+ * Description of FSSlot
+ *
+ * @author Lauric Francelet
+ */
+class FSSlot {
+    /**
+     * The constructor that does nothing
+     */
+    public function __construct() {
+        // Nothing
+    }
+    
+    /**
+     * Returns a Message containing a Slot 
+     * @param type $args A Slot No and an Event
+     * @return Message containing the Slot
+     */
+    public static function getSlot($args){
+        global $crud;
+        $slot = NULL;
+        $event = $args['event'];
+        $return = NULL;
+        
+        $sql = "SELECT * FROM Slot WHERE No = ".$args['no']." AND EventNo = ". $event->getNo();
+        $data = $crud->getRow($sql);
+        
+        
+        if($data){
+            $argsSlot = array(
+                'no'            => $data['No'],
+                'eventNo'       => $data['EventNo'],
+                'happeningDate' => $data['HappeningDate'],
+                'startingTime'  => $data['StartingTime'],
+                'endingTime'    => $data['EndingTime'],
+                'isArchived'    => $data['IsArchived'],
+            );
+            
+            $slot = new Slot($argsSlot);
+            
+            $argsMessage = array(
+                'messageNumber' => 115,
+                'message'       => 'Existant Slot',
+                'status'        => true,
+                'content'       => $slot
+            );
+            $return = new Message($argsMessage);
+        } else {
+            $argsMessage = array(
+                'messageNumber' => 116,
+                'message'       => 'Inexistant Slot',
+                'status'        => false,
+                'content'       => NULL
+            );
+            $return = new Message($argsMessage);
+        }
+        
+        return $return;
+    }
+    
+    /**
+     * Returns all the Slots of a given Event
+     * @param an Event
+     * @return a Message conainting an array of Slots
+     */
+    public static function getSlotsByEvent($event){
+        global $crud;
+        
+        $sql = "SELECT * FROM Slot WHERE EventNo = ".$event->getNo();
+        $data = $crud->getRows($sql);
+        
+        if ($data){
+            $slots = array();
+
+            foreach($data as $row){
+                $argsSlot = array(
+                    'no'            => $row['No'],
+                    'eventNo'       => $row['EventNo'],
+                    'happeningDate' => $row['HappeningDate'],
+                    'startingTime'  => $row['StartingTime'],
+                    'endingTime'    => $row['EndingTime'],
+                    'isArchived'    => $row['IsArchived'],
+                );
+            
+                $slots[] = new Slot($argsSlot);
+            } //foreach
+
+            $argsMessage = array(
+                'messageNumber' => 117,
+                'message'       => 'All Slots for an Event selected',
+                'status'        => true,
+                'content'       => $slots
+            );
+            
+            $return = new Message($argsMessage);
+
+        } else {
+            $argsMessage = array(
+                'messageNumber' => 118,
+                'message'       => 'Error while SELECT * FROM person',
+                'status'        => false,
+                'content'       => NULL
+            );
+            
+            $return = new Message($argsMessage);
+
+        }
+        
+        return $return;
+    }
+    
+    /**
+     * Returns all the Slots
+     * @return a Message conainting an array of Slots
+     */
+    public static function getSlots(){
+        global $crud;
+        
+        $sql = "SELECT * FROM Slot";
+        $data = $crud->getRows($sql);
+        
+        if ($data){
+            $slots = array();
+
+            foreach($data as $row){
+                $argsSlot = array(
+                    'no'            => $row['No'],
+                    'eventNo'       => $row['EventNo'],
+                    'happeningDate' => $row['HappeningDate'],
+                    'startingTime'  => $row['StartingTime'],
+                    'endingTime'    => $row['EndingTime'],
+                    'isArchived'    => $row['IsArchived'],
+                );
+            
+                $slots[] = new Slot($argsSlot);
+            } //foreach
+
+            $argsMessage = array(
+                'messageNumber' => 119,
+                'message'       => 'All Slots selected',
+                'status'        => true,
+                'content'       => $slots
+            );
+            
+            $return = new Message($argsMessage);
+
+        } else {
+            $argsMessage = array(
+                'messageNumber' => 120,
+                'message'       => 'Error while SELECT * FROM person',
+                'status'        => false,
+                'content'       => NULL
+            );
+            
+            $return = new Message($argsMessage);
+
+        }
+        
+        return $return;
+    } 
+
+    /*
+    public static function getUnitByName($aName) {
+        $unit = NULL;
+        
+        global $crud;
+        
+        $sql = "SELECT * FROM Unit WHERE Unit.Name = '" . $aName . "'";
+        $data = $crud->getRow($sql);
+        
+        if($data){
+            $argsUnit = array(
+                'no'      => $data['No'],
+                'name'        => $data['Name'],
+                'isArchived'    => $data['IsArchived'],
+            );
+            
+            $unit = new Unit($argsUnit);
+            
+            $argsMessage = array(
+                'messageNumber' => 400,
+                'message'       => 'Existant Unit',
+                'status'        => true,
+                'content'       => $unit
+            );
+            $message = new Message($argsMessage);
+            return $message;
+        } else {
+            $argsMessage = array(
+                'messageNumber' => 401,
+                'message'       => 'Inexistant Unit',
+                'status'        => false,
+                'content'       => NULL
+            );
+            $message = new Message($argsMessage);
+            return $message;
+        }
+    }*/
+    
+    /**
+     * Returns all the units of a member, or NULL if the member doesn't have 
+     * any units
+     * @global Crud $crud A Crud Object
+     * @param Member $member The member we are getting the units for.
+     * @return Message The Units for a member
+     */
+    /*
+    public static function getAllUnitsForMember(Member $member) {
+        $unit = NULL;
+        
+        global $crud;
+        
+        $sql = "SELECT Unit.Name FROM Member
+            INNER JOIN Membership
+            ON Member.ID = Membership.MemberID
+            INNER JOIN Unit
+            ON Membership.UnitNo = Unit.No
+            WHERE Member.ID = '" . $member->getId() . "'";
+        
+        $data = $crud->getRows($sql);
+        
+        // If we got the units, we give them back to the caller
+        if( $data ) {
+            
+            $units = array();
+            
+            foreach( $data as $unit ) {
+                $units[] = $unit['Name'];
+            }
+            
+            return $units;
+        }
+        // Else : we give NULL back
+        else {
+            return NULL;
+        }
+    }*/
+}
+
+?>
--- dev/LF_test.php
+++ dev/LF_test.php
@@ -10,6 +10,8 @@ require_once(APP_DIR .'/core/services/functionnals/FSPerson.class.php');
 require_once(APP_DIR .'/core/services/functionnals/FSMembership.class.php');
 require_once(APP_DIR .'/core/services/functionnals/FSUnit.class.php');
 require_once(APP_DIR .'/core/services/functionnals/FSMember.class.php');
+require_once(APP_DIR .'/core/services/functionnals/FSEvent.class.php');
+require_once(APP_DIR .'/core/services/functionnals/FSSlot.class.php');
 require_once(APP_DIR .'/core/model/Member.class.php');
 require_once(APP_DIR .'/core/model/Unit.class.php');
 
@@ -20,16 +22,32 @@ require_once(APP_DIR .'/core/model/Unit.class.php');
 
 
 
-$member = FSMember::getMember('admin')->getContent();
-var_dump($member);
+//$member = FSMember::getMember('admin')->getContent();
+//var_dump($member);
 
-$unit = FSUnit::getUnit(2)->getContent();
-var_dump($unit);
+//$unit = FSUnit::getUnit(2)->getContent();
+//var_dump($unit);
 
-$argsMembership = array ('member' => $member, 'unit' => $unit);
+//$argsMembership = array ('member' => $member, 'unit' => $unit);
 
 //var_dump(FSMembership::getMembership($argsMembership));
-var_dump(FSMembership::addMembership($argsMembership));
+//var_dump(FSMembership::addMembership($argsMembership));
+
+$event = FSEvent::getEvent(1)->getContent();
+var_dump($event);
+
+$argsSlot = array (
+    'no'    => '1',
+    'event' => $event
+);
+
+$slot = FSSlot::getSlot($argsSlot)->getContent();
+var_dump($slot);
+
+var_dump(FSSlot::getSlotsByEvent($event));
+
+var_dump(FSSlot::getSlots());
+
 
 
 
