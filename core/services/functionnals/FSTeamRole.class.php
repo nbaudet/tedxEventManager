a06e8723a4b1461ccf7e0bf7a1f4f6eac06ed25c
spacefiller <guillaume.lehmann@heig-vd.ch>
2013-06-18 11:56:22 +0200

    Modification FSTeamRole ajout get

--- core/services/functionnals/FSTeamRole.class.php
+++ core/services/functionnals/FSTeamRole.class.php
@@ -1,9 +1,5 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  * Description of FSTeamRole
@@ -11,7 +7,93 @@
  * @author rapou
  */
 class FSTeamRole {
-    //put your code here
+       /**
+     *Returns a TeamRole with the given No as Id
+     *@param string $name the id of the TeamRole
+     *@return a Mesage with an existant TeamRole
+     */
+    public function getTeamRole($name){
+        $location = NULL;
+        
+        global $crud;
+        $name = addslashes($name);
+        $sql = "SELECT * FROM TeamRole WHERE Name = '$name'";
+        $data = $crud->getRow($sql);
+        
+        if($data){
+            $argsLocation = array(
+                'name'          => $data['Name'],
+                'isMemberOf'    => $data['IsMemberOf'],
+                'isArchived'    => $data['IsArchived']
+            );
+        
+            $location = new TeamRole($argsTeamRole);
+
+            $argsMessage = array(
+                'messageNumber'     => 201,
+                'message'           => 'Existant Location',
+                'status'            => true,
+                'content'           => $location
+            );
+            $message = new Message($argsMessage);
+            return $message;
+        }else{
+            $argsMessage = array(
+                'messageNumber'     => 202,
+                'message'           => 'Inexistant TeamRole',
+                'status'            => false,
+                'content'           => NULL    
+            );
+            $message = new Message($argsMessage);
+            return $message;
+        }// else
+    }// function
+
+    /**
+     * Returns all the TeamRoles of the database
+     * @return A Message containing an array of TeamRoles
+     */
+    public function getTeamRoles(){
+        global $crud;
+
+        $sql = "SELECT * FROM TeamRole WHERE IsArchived = 0";
+        $data = $crud->getRows($sql);
+        
+        if ($data){
+            $locations = array();
+
+            
+            foreach($data as $row){
+                $argsLocation = array(
+                    'name'          => $row['Name'],
+                    'isMemberOf'    => $row['IsMemberOf'],
+                    'isArchived'    => $row['IsArchived']
+                );
+            
+                $locations[] = new TeamRole($argsTeamRole);
+            } //foreach
+
+            $argsMessage = array(
+                'messageNumber' => 203,
+                'message'       => 'All TeamRoles selected',
+                'status'        => true,
+                'content'       => $locations
+            );
+            $message = new Message($argsMessage);
+
+            return $message;
+        } else {
+            $argsMessage = array(
+                'messageNumber' => 204,
+                'message'       => 'Error while SELECT * FROM TeamRole',
+                'status'        => false,
+                'content'       => NULL
+            );
+            $message = new Message($argsMessage);
+
+            return $message;
+        }// else
+    }// function
 }
 
 ?>
