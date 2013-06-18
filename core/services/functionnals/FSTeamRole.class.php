48c9285ee4f3635f96f6b2c4fc5dfcbb9bc2f126
Francelet L <lauric.francelet@heig-vd.ch>
2013-06-18 14:19:24 +0200

    resolutionDeOuf

--- core/services/functionnals/FSTeamRole.class.php
+++ core/services/functionnals/FSTeamRole.class.php
@@ -4,7 +4,7 @@
 /**
  * Description of FSTeamRole
  *
- * @author rapou
+ * @author Lauric
  */
 class FSTeamRole {
        /**
@@ -12,8 +12,8 @@ class FSTeamRole {
      *@param string $name the id of the TeamRole
      *@return a Mesage with an existant TeamRole
      */
-    public function getTeamRole($name){
-        $location = NULL;
+    public static function getTeamRole($name){
+        $teamRole = NULL;
         
         global $crud;
         $name = addslashes($name);
@@ -21,79 +21,146 @@ class FSTeamRole {
         $data = $crud->getRow($sql);
         
         if($data){
-            $argsLocation = array(
+            $argsTeamRole = array(
                 'name'          => $data['Name'],
                 'isMemberOf'    => $data['IsMemberOf'],
                 'isArchived'    => $data['IsArchived']
             );
         
-            $location = new TeamRole($argsTeamRole);
+            $teamRole = new TeamRole($argsTeamRole);
 
             $argsMessage = array(
-                'messageNumber'     => 301,
+                'messageNumber'     => 147,
                 'message'           => 'Existant TeamRole',
                 'status'            => true,
                 'content'           => $teamRole
             );
-            $message = new Message($argsMessage);
-            return $message;
+            $return = new Message($argsMessage);
+
         }else{
             $argsMessage = array(
-                'messageNumber'     => 302,
+                'messageNumber'     => 148,
                 'message'           => 'Inexistant TeamRole',
                 'status'            => false,
                 'content'           => NULL    
             );
-            $message = new Message($argsMessage);
-            return $message;
-        }// else
+            $return = new Message($argsMessage);
+
+        }
+        return $return;
     }// function
 
     /**
      * Returns all the TeamRoles of the database
      * @return A Message containing an array of TeamRoles
      */
-    public function getTeamRoles(){
+    public static function getTeamRoles(){
         global $crud;
 
         $sql = "SELECT * FROM TeamRole WHERE IsArchived = 0";
         $data = $crud->getRows($sql);
         
         if ($data){
-            $locations = array();
-
-            
+            $teamRoles = array();
+ 
             foreach($data as $row){
-                $argsLocation = array(
+                $argsTeamRole = array(
                     'name'          => $row['Name'],
                     'isMemberOf'    => $row['IsMemberOf'],
                     'isArchived'    => $row['IsArchived']
                 );
             
-                $locations[] = new TeamRole($argsTeamRole);
+                $teamRoles[] = new TeamRole($argsTeamRole);
             } //foreach
 
             $argsMessage = array(
-                'messageNumber' => 303,
+                'messageNumber' => 151,
                 'message'       => 'All TeamRoles selected',
                 'status'        => true,
                 'content'       => $teamRoles
             );
-            $message = new Message($argsMessage);
+            $return = new Message($argsMessage);
 
-            return $message;
         } else {
             $argsMessage = array(
-                'messageNumber' => 304,
+                'messageNumber' => 152,
                 'message'       => 'Error while SELECT * FROM TeamRole',
                 'status'        => false,
                 'content'       => NULL
             );
-            $message = new Message($argsMessage);
+            $return = new Message($argsMessage);
 
-            return $message;
-        }// else
+        }
+        return $return;
+        
     }// function
+    
+    /**
+     * Checks non existance of new TeamRole to add, and calls the createTeamRole function
+     * @param string $name
+     * @return a Message containing the added TeamRole
+     */
+    public static function addTeamRole($name){
+
+        // Validate Inexistant TeamRole
+        $messageValidateTeamRole = FSTeamRole::addTeamRole($name);
+        
+        // Create new TeamRole 
+        if ($messageValidateTeamRole->getStatus() == FALSE){
+            
+            $messageCreateTeamRole = FSTeamRole::createTeamRole($name);
+            $return = $messageCreateTeamRole;
+            
+        } else {
+            // Existant TeamRole
+            $return = $messageValidateTeamRole;
+        }
+        
+        return $return;
+    } // END addTeamRole
+    
+    /**
+     * Inserts a new TeamRole in database
+     * @param string $name
+     * @return a Message containing the created TeamRole
+     */
+    public static function createTeamRole($name){
+        global $crud;
+        
+        $sql = "INSERT INTO TeamRole (Name) VALUES (
+            $name
+        )";
+        
+        $data = $crud->exec($sql);
+        
+        if ($data){
+                        
+            $messageCreateTeamRole = FSTeamRole::getTeamRole($name);
+            
+            if ($messageCreateTeamRole->getStatus()){
+                $aCreatedTeamRole = $messageCreateTeamRole->getContent();
+                $argsMessage = array(
+                    'messageNumber' => 153,
+                    'message'       => 'New TeamRole added !',
+                    'status'        => true,
+                    'content'       => $aCreatedTeamRole
+            );
+            
+            $return = new Message($argsMessage);
+            
+            } else {
+                $argsMessage = array(
+                    'messageNumber' => 154,
+                    'message'       => 'Error while inserting new TeamRole',
+                    'status'        => false,
+                    'content'       => NULL
+                );
+                $return = new Message($argsMessage);
+            }
+            return $return;                   
+        }
+    } // END createTeamRole
+    
 }
 
 ?>
