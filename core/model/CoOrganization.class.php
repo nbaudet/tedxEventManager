1c92e488a32d4c2fa255b289fe1679f984d2ba77
Francelet L <lauric.francelet@heig-vd.ch>
2013-06-19 15:34:51 +0200

    Set CoOrg

--- core/model/CoOrganization.class.php
+++ core/model/CoOrganization.class.php
@@ -25,8 +25,25 @@ class Talk {
      * Talk's speakerPersonNo
      * @var type int 
      */
-    private $speakerPersonNo; 
+    private $speakerPersonNo;
     
+    /**
+     * Talk's videoTalk
+     * @var type int 
+     */
+    private $videoTitle;
+    
+    /**
+     * Talk's videoDescription
+     * @var type int 
+     */
+    private $videoDescription;
+    
+    /**
+     * Talk's videoURL
+     * @var type int 
+     */
+    private $videoURL;    
     
     /**
      * Talk's isArchived 
@@ -48,14 +65,12 @@ class Talk {
         }//if
         $this->eventNo = $array['eventNo']; 
         $this->speakerPersonNo = $array['speakerPersonNo']; 
+        $this->videoTitle = $array['videoTitle']; 
+        $this->videoDescription = $array['videoDescription']; 
+        $this->videoURL = $array['videoURL']; 
         $this->isArchived = $array['isArchived']; 
-
-        
- 
         
     }//construct
-    
-    
     
     
     
@@ -85,6 +100,29 @@ class Talk {
         return $this->isArchived; 
     }//function
     
+    /**
+     * get videoTitle
+     * @return String videoTitle
+     */
+    public function getVideoTitle() {
+        return $this->videoTitle; 
+    }//function
+    
+    /**
+     * get videoDescription
+     * @return string videoDescription
+     */
+    public function getVideoDescription() {
+        return $this->videoDescription; 
+    }//function
+    
+    /**
+     * get videoURL
+     * @return string videoURL
+     */
+    public function getVideoURL() {
+        return $this->videoURL; 
+    }//function
     
     /**
      * set isArchived
@@ -94,8 +132,29 @@ class Talk {
         $this->isArchived = $isArchived; 
     }//function
     
+    /**
+     * set videoTitle
+     * @param string $videoTitle
+     */
+    public function setVideoTitle($videoTitle) {
+        $this->videoTitle = $videoTitle; 
+    }//function    
     
+    /**
+     * set videoDesritpion
+     * @param string $videoDescription
+     */
+    public function setVideoDescription($videoDescription) {
+        $this->videoDescription = $videoDescription; 
+    }//function   
     
+    /**
+     * set videoURL
+     * @param string $videoURL
+     */
+    public function setVideoURL($videoURL) {
+        $this->videoURL = $videoURL; 
+    }//function   
     
 }//class
 ?>
