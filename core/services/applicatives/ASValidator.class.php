5893863a3810abc0d7af3da68d87bd2a2f1cd7de
Nicolas Baudet <nicolas.baudet@heig-vd.ch>
2013-06-24 16:42:20 +0200

    Commit de Nicolas Baudet

--- core/services/applicatives/ASValidator.class.php
+++ core/services/applicatives/ASValidator.class.php
@@ -28,79 +28,62 @@ require_once(APP_DIR . '/core/services/functionnals/FSPerson.class.php');
  * @author rapou
  */
 class ASValidator {
+
     /**
      * The constructor that does nothing
      */
     public function __construct() {
         // Nothing
     }
-    
+
     // Change the status of the current registration.
     private static function changeRegistrationStatus($args) {
         $currentRegistration = $args['currentRegistration'];
         $newStatus = $args['newStatus'];
-        //If event not empty
-        if(isset($currentRegistration['event'])){
-                        $messageValidEvent = FSEvent::getEvent($currentRegistration->getEventNo());
-                        if($messageValidEvent->getStatus()){
-                            $aValidEvent = $messageValidEvent->getContent();
-                                if(isset($currentRegistration['participant'])){
-                                        $messageValidParticipant = FSParticipant::getParticipant($currentRegistration->getParticipantPersonNo());
-                                        if($messageValidParticipant->getStatus()){
-                                            $aValidParticipant = $messageValidParticipant->getContent();
-                                            $currentRegistration->setIsArchived(1);
-                                            $messageArchiveRegistration = FSRegistration::archiveRegistration($currentRegistration);
-                                            if($messageArchiveRegistration->getStatus()){
-                                                 $argsRegistration = array(
-                                                    'status'          => $newStatus, // String
-                                                    'type'            => $currentRegistration->getType(), // String
-                                                    'typeDescription' => $currentRegistration->getTypeDescription(), // Optionel - String
-                                                    'event'           => $aValidEvent, // object Event
-                                                    'participant'     => $aValidParticipant  // object Participant
-                                                );
-                                                $message = FSRegistration::addRegistration($argsRegistration);
-                                            }else{
-                                                $message = $messageArchiveRegistration;
-                                            }
-                                        }else{
-                                            $message = $messageValidParticipant;
-                                        }
-                                }else{
-                                    $argsMessage = array(
-                                        'messageNumber'     => 208,
-                                        'message'           => 'Inexistant Participant',
-                                        'status'            => false,
-                                        'content'           => NULL 
-                                    );
-                                    $message= new Message($argsMessage);
-                                }
-                        }else{
-                            $message = $messageValidEvent;
-                        }
-                }else{
-                    $argsMessage = array(
-                        'messageNumber'     => 212,
-                        'message'           => 'Inexistant Event',
-                        'status'            => false,
-                        'content'           => null
+
+        $messageValidEvent = FSEvent::getEvent($currentRegistration->getEventNo());
+        if ($messageValidEvent->getStatus()) {
+            $aValidEvent = $messageValidEvent->getContent();
+            $messageValidParticipant = FSParticipant::getParticipant($currentRegistration->getParticipantPersonNo());
+            if ($messageValidParticipant->getStatus()) {
+                $aValidParticipant = $messageValidParticipant->getContent();
+                $currentRegistration->setIsArchived(1);
+                $messageArchiveRegistration = FSRegistration::archiveRegistration($currentRegistration);
+                if ($messageArchiveRegistration->getStatus()) {
+                    $argsRegistration = array(
+                        'status' => $newStatus, // String
+                        'type' => $currentRegistration->getType(), // String
+                        'typeDescription' => $currentRegistration->getTypeDescription(), // Optionel - String
+                        'event' => $aValidEvent, // object Event
+                        'participant' => $aValidParticipant  // object Participant
                     );
-                    $message= new Message($argsMessage);
+                    $message = FSRegistration::addRegistration($argsRegistration);
+                } else {
+                    $message = $messageArchiveRegistration;
                 }
-        return $message; 
-    }//function
-    
+            } else {
+                $message = $messageValidParticipant;
+            }
+        } else {
+            $message = $messageValidEvent;
+        }
+
+        return $message;
+    }
+
+//function
     // 
     public static function acceptRegistration($aRegistration) {
         $args = array('currentRegistration' => $aRegistration, 'newStatus' => 'Accepted');
         return self::changeRegistrationStatus($args);
     }
-    
+
     //
     public static function rejectRegistration($aRegistration) {
         $args = array('currentRegistration' => $aRegistration, 'newStatus' => 'Rejected');
         return self::changeRegistrationStatus($args);
     }
-    
+
 }
 
 ?>
